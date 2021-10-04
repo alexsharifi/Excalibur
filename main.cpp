51241b588fb675f1cd348247239be553b5504755
Alex Sharifi <Alexandersharifi@gmail.com>
2021-10-04 13:19:58 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>;
+#include <vector>;
+
+using namespace std;
+
+int mathplus(int n1, int n2){
+  int n3 = n1 + n2;
+  return n3:
+}
+
+void main(){
+  cout << mathplus() <<endl;
+  
+}
