e6d94d6b6151ecb11a3f66cb4913891833fd33b1
Alex Sharifi <Alexandersharifi@gmail.com>
2021-10-04 13:24:36 -0700

    oop

--- main.cpp
+++ main.cpp
@@ -3,18 +3,34 @@
 
 using namespace std;
 
-int mathplus(int n1, int n2){
-  int n3 = n1 + n2;
-  return n3:
+class mathfunc{
+ private:
+  int errorcode;
+  
+ public:
+  int mathplus(int n1, int n2){
+    int n3 = n1 + n2;
+    return n3:
 }
-
-int mathminus(int n1,int n2){
-  int n3 = n1 - n2;
-  return n3;
+  
+  
+  int mathminus(int n1,int n2){
+     int n3 = n1 - n2;
+     return n3;
   
 }
+  
+  
+}  
+  
+
+
+
+
 
 void main(){
+  
+  
   cout << mathplus() <<endl;
   
 }
